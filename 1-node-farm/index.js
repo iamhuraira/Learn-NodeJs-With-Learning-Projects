d0dd7d23664c7e243cf368100cbaad45ba92a7e0
Abu Huraira <iamhuraira@gmail.com>
2022-09-30 18:36:48 +0500

    Create Server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const url = require('url');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+////File  read and Write
+/* const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
  
 console.log(textIn)
 
@@ -8,4 +12,28 @@ const textOut = `This is what we know about the avocado: ${textIn}.\n Created on
 
 fs.writeFileSync('./txt/output.txt', textOut)
 
-console.log('File Written')
+console.log('File Written') */
+
+
+///////////////////
+
+////    SERVER /////////
+const server =http.createServer((req, res)=>{
+    // console.log(req.url);
+    // console.log(req);
+    const  patName = req.url;
+    if(patName === '/' || patName==='/overview'){
+        res.end('This is the Overview');
+    }
+    else if(patName === '/product'){
+          res.end('This is the Product');
+    }
+    else{
+        res.end("Page Not Found")
+    }
+    res.end('Hello From The Server')
+}); 
+server.listen(8000,'127.0.0.1', ()=>{
+    console.log('Listening to request on port 8000')
+})
+
